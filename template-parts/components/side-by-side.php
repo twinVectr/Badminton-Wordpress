765fb9d00d0ea129ca55536e704d53ac364ac712
Ahilan Gopalakrishnan <ahilangopal@gmail.com>
2019-03-30 00:01:31 -0400

    Membership Component

---
+++ template-parts/components/side-by-side.php
@@ -0,0 +1,15 @@
+<div class="section-membership" style="height: 500px">
+  <div class="row vSpace"></div>
+  <div class="row">
+    <div class="col-xs-12 col-md-5 textCenter">
+      <h1 class="no-padding bold"> <?=$leftSideContent?></h1>
+    </div>
+    <div class="col-xs-12 col-md-1"></div>
+    <div class="col-xs-12 col-md-5">
+      <p><?=$rightSideDescription?></p>
+      <div>
+      </div>
+    </div>
+  </div>
+  <div class="row vSpace"></div>
+</div>
--- template-parts/page-sections/home-membership.php
+++ template-parts/page-sections/home-membership.php
@@ -1,15 +1,6 @@
 <?php
-$membershipText = "Membership";
-?>
 
-<div class="section-membership" style="height: 500px">
-  <div class="row">
-    <div class="col-xs-12 col-md-6">
-      <h1> <?=$membershipText?></h1>
-    </div>
-    <div class="col-xs-12 col-md-5">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-  </div>
-</div>
+$leftSideContent = 'Membership';
+$rightSideDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
+include locate_template('template-parts/components/side-by-side.php', false, false);
