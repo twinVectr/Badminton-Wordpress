5f84d7efd3d43b5148a0d947e168658871290659
Ahilan Gopalakrishnan <ahilangopal@gmail.com>
2019-04-05 22:24:07 -0400

    changes_membership

--- template-parts/components/side-by-side.php
+++ template-parts/components/side-by-side.php
@@ -1,4 +1,4 @@
-<div class="section-membership" style="height: 500px">
+<div class="section-membership">
   <div class="row vSpace"></div>
   <div class="row">
     <div class="col-xs-12 col-md-5 textCenter membership--left">
@@ -7,11 +7,11 @@
     <div class="col-xs-12 col-md-1"></div>
     <div class="col-xs-12 col-md-5 membership--right">
       <p><?=$sectionContentSplit[1]?></p>
-      <div class="row paddingTop25">
-        <div class="col-xs-5 membership-price-right">
+      <div class="row paddingTop25 bold">
+        <div class="col-xs-5">
           <p><?=$sectionContentSplit[2]?></p>
         </div>
-        <div class="col-xs-5 membership-price-left">
+        <div class="col-xs-5">
           <p><?=$sectionContentSplit[3]?></p>
         </div>
       </div>
