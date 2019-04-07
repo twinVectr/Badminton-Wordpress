481aef7734b3327871222a19030df5d13f02fb0f
twinVectr <gokul2259@gmail.com>
2019-04-06 20:03:20 -0400

    added a trainer card markup

---
+++ template-parts/page-sections/trainers-profile.php
@@ -0,0 +1,18 @@
+<div class="WM-section vertical-center">
+  <div class="container">
+    <div class="trainers-card">
+      <div class="trainer-profile-img"></div>
+      <p class="trainer-name paddingBottom10"> Murli Vijay </p>
+      <p class="bold">Specifications</p>
+      <p>Sports Conditioning</p>
+      <p>Weight Management</p>
+      <p>Strength Traning</p>
+      <p class="paddingTop10">
+        Murali Vijay is an Indian international cricketer who plays as a right-handed opening batsman. A regular member
+        of
+        the Indian Test team, he also represents Tamil Nadu in first-class cricket and Chennai Super Kings in the Indian
+        Premier League.
+      </p>
+    </div>
+  </div>
+</div>
--- templates/home-page.php
+++ templates/home-page.php
@@ -7,5 +7,6 @@ get_template_part('template-parts/page-sections/home', 'hero');
 get_template_part('template-parts/page-sections/home', 'membership');
 get_template_part('template-parts/page-sections/home', 'image-banner');
 get_template_part('template-parts/page-sections/home', 'join-today');
-
+get_template_part('template-parts/page-sections/trainers', 'profile');
+\
 get_footer();
