cf51073b6e50921c9020fe2c72af5790cc9a5f6e
twinVectr <gokul2259@gmail.com>
2019-04-06 20:14:33 -0400

    ACF update

--- ACFSettings.php
+++ ACFSettings.php
@@ -1,4 +1,5 @@
 <?php
+
 if (function_exists('acf_add_local_field_group')):
 
     acf_add_local_field_group(array(
@@ -28,9 +29,52 @@ if (function_exists('acf_add_local_field_group')):
         'location' => array(
             array(
                 array(
-                    'param' => 'page_type',
+                    'param' => 'page',
                     'operator' => '==',
-                    'value' => 'top_level',
+                    'value' => '2',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5ca913ef7fb88',
+        'title' => 'Open hours',
+        'fields' => array(
+            array(
+                'key' => 'field_5ca9141d7b2a4',
+                'label' => 'Open hours',
+                'name' => 'open_hours',
+                'type' => 'wysiwyg',
+                'instructions' => '',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 1,
+                'delay' => 0,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'page',
+                    'operator' => '==',
+                    'value' => '2',
                 ),
             ),
         ),
