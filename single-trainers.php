494b8f17110efbf75be6a2328d4a992519b3d778
twinVectr <gokul2259@gmail.com>
2019-05-04 16:09:21 -0400

    trainers fix

--- single-trainers.php
+++ single-trainers.php
@@ -6,8 +6,7 @@ $current_post = get_post();
 <div class="container">
   <div class="vSpace"></div>
   <div class="container trainer-details" style="height: auto; line-height: 1.5em">
-    <img src="<?=get_the_post_thumbnail_url($current_post)?>" class="floatLeft marginRight20 marginBottom20 paddingTop5"
-      width="300" />
+    <img src="<?=get_the_post_thumbnail_url($current_post)?>" class="trainer-img" />
     <p>
       <?=$current_post->post_content?>
     </p>
