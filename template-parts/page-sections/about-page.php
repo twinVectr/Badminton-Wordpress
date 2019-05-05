eaaa14933db6aff7f3950be6bcbf0bb22cec932e
Ahilan Gopalakrishnan <ahilangopal@gmail.com>
2019-05-04 20:07:39 -0400

    --Cleaned up the about page

--- template-parts/page-sections/about-page.php
+++ template-parts/page-sections/about-page.php
@@ -7,21 +7,14 @@ $sectionContent = get_field('location-hours-classes');
 $sectionContentData = explode("@@@@", $sectionContent);
 
 $sectionContentOneSplit = explode("####", $sectionContentData[0]);
-$sectionContentTwoSplit = explode("####", $sectionContentData[1]);
 
 $sectionContentOne = array(
-    'leftContent' => $sectionContentOneSplit[0],
-    'rightContent' => $sectionContentOneSplit[1],
-    'sideBySideLeft' => $sectionContentOneSplit[2],
-    'sideBySideRight' => $sectionContentOneSplit[3]);
-
-$sectionContentTwo = array(
-    'leftContent' => $sectionContentTwoSplit[0],
-    'rightSideBySideLeft' => $sectionContentTwoSplit[1],
-    'rightSideBySideRight' => $sectionContentTwoSplit[2]);
+  'leftContent' => $sectionContentOneSplit[0],
+  'rightContent' => $sectionContentOneSplit[1],
+  'sideBySideLeft' => $sectionContentOneSplit[2],
+  'sideBySideRight' => $sectionContentOneSplit[3]
+);
 
 $children = $mustache->render('about', array('sectionContentSplit' => $sectionContentOne));
-
-$childrenTwo = $mustache->render('about.subcontent', array('sectionContentSplit' => $sectionContentTwo));
 
 include locate_template('template-parts/components/about-page.php', false, false);
