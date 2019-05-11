935ab88a11a1bf83e5029dc00200a021a3915d34
twinVectr <gokul2259@gmail.com>
2019-05-11 16:51:14 -0400

    parallax image fix

--- template-parts/components/image-banner.php
+++ template-parts/components/image-banner.php
@@ -1,5 +1,14 @@
 <div class="WM-section section-banner-1 vertical-center">
+  <?php if (wp_is_mobile()) {?>
   <div class="WM-background-image-container" style="background-image: url('<?=$backgroundImage?>')"></div>
+
+  <?php } else {?>
+
+  <div data-jarallax data-speed="0.2" class="jarallax WM-background-image-container"
+    style="background-image: url('<?=$backgroundImage?>')"></div>
+
+  <?php }?>
+
   <div class="container">
     <div class="row">
       <?=$children?>
--- template-parts/components/image-context.php
+++ template-parts/components/image-context.php
@@ -1,5 +1,13 @@
 <div class="WM-section section-hero vertical-center">
+  <?php if (wp_is_mobile()) {?>
   <div class="WM-background-image-container" style="background-image: url('<?=$backgroundImage?>')"></div>
+
+  <?php } else {?>
+
+  <div data-jarallax data-speed="0.2" class="jarallax WM-background-image-container"
+    style="background-image: url('<?=$backgroundImage?>')"></div>
+
+  <?php }?>
   <div class="container">
     <div class="row">
       <?=$children?>
